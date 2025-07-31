61a5188d44c7dc2bcfe845fff1f3dc4301ce2480
S SIDHARTH <sidharthpunalur@gmail.com>
2025-07-31 18:55:41 +0530

    cookie persistance added

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,14 +1,15 @@
 import Cookies from "js-cookie";
 
+
 export const setTokens = (accessToken, refreshToken) => {
   Cookies.set('accessToken', accessToken, { expires: 1 }); 
-  Cookies.set('refreshToken', refreshToken, { expires: 30 });
+  Cookies.set('refreshToken', refreshToken, { expires: 30 }); 
 };
 
-export const getAccessToken = () => localStorage.getItem('accessToken');
-export const getRefreshToken = () => localStorage.getItem('refreshToken');
+export const getAccessToken = () => Cookies.get('accessToken');
+export const getRefreshToken = () => Cookies.get('refreshToken');
 
 export const clearTokens = () => {
-  localStorage.removeItem('accessToken');
-  localStorage.removeItem('refreshToken');
+  Cookies.remove('accessToken');
+  Cookies.remove('refreshToken');
 };
