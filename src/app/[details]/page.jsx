fae8ce9de3c1dfdd78c7992c2cb3871d55a1f75f
S SIDHARTH <sidharthpunalur@gmail.com>
2025-08-01 14:05:11 +0530

    base structure added

---
+++ src/app/[details]/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+
+      {/* import details component from /components/details/Footer.jsx */}
+    </div>
+  )
+}
